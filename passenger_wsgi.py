c92207f0ffd5e9033afd2cf2ba2eaba6b5295df6
Shoaib <shoaibshoukatbhatti6252@gmail.com>
2026-07-21 20:48:16 +0500

    new update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,35 +1,24 @@
-"""
-WSGI config for cPanel deployment
-This file is used by cPanel's Python application to serve the Django backend
-
-DEPLOYMENT INSTRUCTIONS:
-1. Upload backend folder to: /home/tmfouzys/backend.tmfouzy.sg/
-2. Update project_home path below to match your cPanel username
-3. Install requirements: pip install -r requirements.txt
-4. Run migrations: python manage.py migrate
-5. Collect static files: python manage.py collectstatic
-6. Create superuser: python manage.py createsuperuser
-"""
-
-import os
 import sys
+import os
 
-# Add your project directory to the sys.path
-# IMPORTANT: Update 'tmfouzys' to your actual cPanel username
-project_home = '/home/tmfouzys/backend.tmfouzy.sg'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+# Virtualenv Python path (matches .htaccess Python version 3.9)
+INTERP = "/home/tmfouzys/virtualenv/backend.tmfouzy.sg/3.9/bin/python"
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
 
-# Set environment variable to tell Django where settings are
-os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
+# Project root path
+sys.path.insert(0, '/home/tmfouzys/backend.tmfouzy.sg')
 
-# Load environment variables from .env file
+# Set Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# Load .env file
 try:
     from dotenv import load_dotenv
-    load_dotenv(os.path.join(project_home, '.env'))
+    load_dotenv('/home/tmfouzys/backend.tmfouzy.sg/.env')
 except ImportError:
-    pass  # python-dotenv not installed
+    pass
 
-# Import Django's WSGI application
+# Django WSGI application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
